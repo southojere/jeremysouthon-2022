e80604f74894e37c9d2a94cccd4de4a30b4ddaf0
jeremy southon <jsouthon@canva.com>
2022-02-06 11:55:31 +1300

    feat: add to home page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
+import { Footer } from '@/components/layout/Footer';
 import { PortraitHeader } from '@/components/layout/Header';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
 import { AboutSection } from './About';
+import { ContactSection } from './Contact';
 import { IntroSection } from './Intro';
 import { PortfolioSection } from './Portfolio';
 import { ServicesSection } from './Services';
@@ -19,6 +21,8 @@ export const Home = () => {
         <PortfolioSection />
         <ServicesSection />
         <AboutSection />
+        <ContactSection />
+        <Footer />
       </main>
     </Layout>
   );
